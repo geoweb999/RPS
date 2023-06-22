ab5aa3c6ee0bbf7e155f386a26d088c40f42a4a9
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-06-22 10:55:05 -0700

    initial refactoring, testing rock

--- rps.js
+++ rps.js
@@ -1,25 +1,22 @@
-function playRound () {
-    // your code here!
-
-  function getUserSelection () {
-    let goodInput = false;
-    let choices = new Set(["rock", "paper", "scissor"]);
-    while (!goodInput) {
-        var choice = prompt("Enter 'rock', 'paper', or 'scissor': ");
-        choice = choice.toLowerCase();
-        goodInput = choices.has(choice);
-    }
-    return choice;
+function getUserSelection () {
+  let goodInput = false;
+  let choices = new Set(["rock", "paper", "scissor"]);
+  while (!goodInput) {
+      var choice = prompt("Enter 'rock', 'paper', or 'scissor': ");
+      choice = choice.toLowerCase();
+      goodInput = choices.has(choice);
   }
+  return choice;
+}
 
-  function getComputerSelection () {
-    let index = Math.floor(Math.random() * 3);
-    let choices = new Array("rock", "paper", "scissor");
-    return choices[index];
-  }
-   
-  let playerSelection = getUserSelection();
-  let computerSelection = getComputerSelection();
+function getComputerSelection () {
+  let index = Math.floor(Math.random() * 3);
+  let choices = new Array("rock", "paper", "scissor");
+  return choices[index];
+}
+  
+
+function checkWinner(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     alert("DRAW: Computer picked " + computerSelection + ", you picked " + playerSelection);
     return 0;
@@ -44,16 +41,42 @@ function playRound () {
   }
 }
 
-let count = 0;
-for (let i = 0; i < 5; i++) {
-    count += playRound();
-}
+var container = document.getElementById('container');
 
-if (count > 0) {
-    alert("Congratulations!  You beat the computer!")
-} else if (count < 0) {
-    alert("Sorry, the computer beat you.")
-} else {
-    alert("it's a tie!")
-}
-  
+var rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+rockButton.addEventListener('click', function() {
+  playerSelection = 'rock';
+  console.log('Player selected: rock');
+  computerSelection = getComputerSelection();
+  console.log('computer selected' + computerSelection);
+  let x = checkWinner(playerSelection, computerSelection);
+
+});
+
+// Create the paper button
+var paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+rockButton.addEventListener('click', function() {
+  playerSelection = 'paper';
+  console.log('Player selected: rock');
+  computerSelection = getComputerSelection();
+
+});
+
+// Create the scissors button
+var scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
+rockButton.addEventListener('click', function() {
+  playerSelection = 'scissor';
+  console.log('Player selected: rock');
+  computerSelection = getComputerSelection();
+
+});
+
+// Append the buttons to the container
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+
+
